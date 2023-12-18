ce55db04d8249f6dc860314ea608cf9720b4a01c
tfiamietsh <ya.touka2017@yandex.ru>
2023-12-18 14:26:57 +0300

    Fix 404 error interception

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -2,15 +2,18 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/c
 import { Injectable } from "@angular/core";
 import { AuthenticationService } from '../services/authentication.service';
 import { Observable, catchError, throwError } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthenticationService) { }
+    constructor(private authService: AuthenticationService, private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(catchError(error => {
             if ([401, 403].includes(error.status) && this.authService.user)
                 this.authService.logout();
+            else if (error.status == 404)
+                this.router.navigate(['/not-found']);
             return throwError(() => error.error.message || error.statusText);
         }
         ));
